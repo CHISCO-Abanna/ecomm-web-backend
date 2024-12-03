943a52e661d101b52080e9f9325022a4f995355e
CHISCO-Abanna <brancketchisco@gmail.com>
2024-12-03 12:52:04 +0100

    updated foldr

--- j24ecom_api/server.js
+++ j24ecom_api/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(
   cors({
     // origin: "http://localhost:5173",
-    origin: "https://ecomm-web-frontend-amber.vercel.app/products",
+    origin: "https://ecomm-web-frontend-amber.vercel.app",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization", "auth-token"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
